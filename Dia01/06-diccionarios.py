01cca4450bb2cfa207e340a37efbb63484c39bc1
Diego Del Castillo <delcastillodsd@gmail.com>
2024-07-08 15:52:52 -0500

    Dia01

--- Dia01/06-diccionarios.py
+++ Dia01/06-diccionarios.py
@@ -9,6 +9,10 @@ pais = input("Ingrese el pais: ")
 if pais in capitales:
     capital = capitales[pais]
     print(f"La capital de {pais} es {capital}")
+    eliminar_capital = input ('¿Desea eliminar la capita (si,no)?')
+    if eliminar_capital == 'si':
+        capitales.pop(pais)
+        print(capitales)
 else:
     print(f'no se encontro la capital de {pais}')
     nueva_capital = input(f'Ingrese la capital de {pais}:   ')
